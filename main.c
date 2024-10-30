007a8ad3457721b96dafb4eec84ae0ec57802ce1
wifren <wifren@gmail.com>
2024-10-30 17:36:19 +0500

    Use proper grammar.

--- main.c
+++ main.c
@@ -7,5 +7,5 @@ int main(int argc, char **argv)
  fgets(name, 255, stdin);
  printf("length = %d\n", strlen(name)); /* debug line */
  name[strlen(name)-1] = '\0'; /* remove the newline at the end */
- printf("Hello %s!\n", name);
+ printf("Hello, %s!\n", name);
  return 0;
